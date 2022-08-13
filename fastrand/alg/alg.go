1b5c0528e2fe5805b0438fd48ecc801b92072261
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-14 08:21:35 +0900

    Add XoShiro256PlusPlus to alg.go

--- fastrand/alg/alg.go
+++ fastrand/alg/alg.go
@@ -1,7 +1,14 @@
 package alg
 
-import "github.com/lemon-mint/experiment/fastrand/alg/splitmix64"
+import (
+	"github.com/lemon-mint/experiment/fastrand/alg/splitmix64"
+	"github.com/lemon-mint/experiment/fastrand/alg/xoshiro256plusplus"
+)
 
 func Splitmix64() uint64 {
 	return splitmix64.Next()
+}
+
+func XoShiro256PlusPlus() uint64 {
+	return xoshiro256plusplus.Next()
 }
