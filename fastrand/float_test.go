09247559de2f7b3abbd7b423afe07b3d1b49271e
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-13 13:09:49 +0900

    Add Test

--- fastrand/float_test.go
+++ fastrand/float_test.go
@@ -2,6 +2,24 @@ package fastrand
 
 import "testing"
 
+func TestFloat64Range(t *testing.T) {
+	for i := 0; i < 1000000; i++ {
+		f := Float64()
+		if !(f < 1 && f >= 0) {
+			t.Fatalf("got %f, want [0, 1)", f)
+		}
+	}
+}
+
+func TestFloat32Range(t *testing.T) {
+	for i := 0; i < 1000000; i++ {
+		f := Float32()
+		if !(f < 1 && f >= 0) {
+			t.Fatalf("got %f, want [0, 1)", f)
+		}
+	}
+}
+
 func BenchmarkFloat64(b *testing.B) {
 	b.RunParallel(func(p *testing.PB) {
 		rng := AcquireRNG()
