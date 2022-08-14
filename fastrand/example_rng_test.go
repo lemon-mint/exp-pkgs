b9a968371506e42950362f8621403fd856056b01
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-14 11:48:06 +0900

    Add Output to Example_rNG

--- fastrand/example_rng_test.go
+++ fastrand/example_rng_test.go
@@ -12,6 +12,13 @@ func Example_rNG() {
 
 	rng.SetSeed(42)
 
+	fmt.Printf("Number between 0 and 10: %d\n", rng.Int63n(10))
 	fmt.Printf("Number between 0 and 100: %d\n", rng.Int63n(100))
+	fmt.Printf("Number between 0 and 1000: %d\n", rng.Int63n(1000))
 	fmt.Printf("64-bit unsigned integer: %d\n", rng.Uint64())
+
+	// Output: Number between 0 and 10: 4
+	// Number between 0 and 100: 67
+	// Number between 0 and 1000: 496
+	// 64-bit unsigned integer: 16395596082725179435
 }
