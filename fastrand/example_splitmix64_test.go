e9db15208192007ec4556ad76272d0e61c2b0c67
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-14 10:19:06 +0900

    Add Output

--- fastrand/example_splitmix64_test.go
+++ fastrand/example_splitmix64_test.go
@@ -12,4 +12,10 @@ func Example_splitmix64() {
 	fmt.Println(alg.Splitmix64())
 	fmt.Println(alg.Splitmix64())
 	fmt.Println(alg.Splitmix64())
+
+	// Output: 9910846700779238901
+	// 5865067318551043833
+	// 4341075433095661249
+	// 9570429342510947219
+	// 10923570614615297618
 }
