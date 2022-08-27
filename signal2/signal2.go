cbef6c4303cb082e2da9e54801f8ee41adf8e3b2
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-27 18:31:38 +0900

    fix: signal.Stop()

--- signal2/signal2.go
+++ signal2/signal2.go
@@ -11,6 +11,8 @@ func WaitForInterrupt() {
 	var ch chan os.Signal = make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
 	<-ch
+	signal.Stop(ch)
 }
 
+// WFI waits for an os.Interrupt (Ctrl+C) or SIGTERM (kill) and then return
 func WFI() { WaitForInterrupt() }
