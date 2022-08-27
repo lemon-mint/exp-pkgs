0f10098aa971304b56fb8f8d96535563d55b5da1
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2022-08-27 12:54:52 +0900

    Why does this code work?

---
+++ primitive/types/go.go
@@ -0,0 +1,16 @@
+package types
+
+type Go_int = int
+type Go_int8 = int8
+type Go_int16 = int16
+type Go_int32 = int32
+type Go_int64 = int64
+
+type Go_uint = uint
+type Go_uint8 = uint8
+type Go_uint16 = uint16
+type Go_uint32 = uint32
+type Go_uint64 = uint64
+
+type Go_float64 = float64
+type Go_float32 = float32
