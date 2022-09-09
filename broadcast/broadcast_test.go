1e1cfb95751531969a6b5db0106fa8df2b934494
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2022-09-09 09:04:11 +0900

    Add Test

---
+++ broadcast/broadcast_test.go
@@ -0,0 +1,25 @@
+package broadcast
+
+import (
+	"sync/atomic"
+	"testing"
+)
+
+func TestTopicBroadcast(t *testing.T) {
+	tt := NewTopic[uint32]()
+	var ctr uint32
+	s := tt.Subscribe(func(v uint32) {
+		atomic.AddUint32(&ctr, v)
+	})
+	for i := 0; i < 100; i++ {
+		s.t.Broadcast(100)
+	}
+	s.Unsubscribe()
+	for i := 0; i < 100; i++ {
+		s.t.Broadcast(100)
+	}
+
+	if ctr != 10000 {
+		t.Fatalf("expected ctr == 10000, got %d", ctr)
+	}
+}
