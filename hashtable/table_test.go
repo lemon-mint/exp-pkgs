0b28506d551ad85e82d53d9255ba6a912d8f896c
lemon-mint <ice.lemons.mint@gmail.com>
2022-09-30 15:37:33 +0900

    Add TestTable

--- hashtable/table_test.go
+++ hashtable/table_test.go
@@ -1,15 +1,18 @@
 package hashtable_test
 
 import (
+	"runtime"
 	"strconv"
 	"sync"
 	"testing"
 
+	"v8.run/go/exp/fastrand"
 	"v8.run/go/exp/hash"
 	"v8.run/go/exp/hashtable"
+	"v8.run/go/exp/util/slice"
 )
 
-const size = 256
+const size = 1 << 20
 
 var keys []string = func() []string {
 	k := make([]string, size)
@@ -19,100 +22,32 @@ var keys []string = func() []string {
 	return k
 }()
 
-func BenchmarkHtabRW(b *testing.B) {
-	t := hashtable.New[string, string](size, hash.MemHashString64)
-	for i := range keys {
-		t.Insert(keys[i], keys[i])
-	}
+func TestTable(t *testing.T) {
+	table := hashtable.New[string, string](size, hash.MemHashString64)
+	cpus := runtime.NumCPU()
+	fastrand.ShuffleSlice(keys)
+	ks := slice.Chunk(keys, size/cpus)
 
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			for i := range keys {
-				t.Lookup(keys[i])
-				t.Insert(keys[i], keys[i])
+	var wg sync.WaitGroup
+	for i := range ks {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+
+			for j := range ks[i] {
+				table.Insert(ks[i][j], ks[i][j])
+				runtime.Gosched()
+				table.Insert(ks[i][j], ks[i][j])
 			}
-		}
-	})
-}
-
-func BenchmarkSyncMapRW(b *testing.B) {
-	t := sync.Map{}
-	for i := range keys {
-		t.Store(keys[i], keys[i])
+		}(i)
 	}
+	wg.Wait()
 
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			for i := range keys {
-				t.Load(keys[i])
-				t.Store(keys[i], keys[i])
-			}
-		}
-	})
-}
-
-func BenchmarkHtabR(b *testing.B) {
-	t := hashtable.New[string, string](size, hash.MemHashString64)
+	fastrand.ShuffleSlice(keys)
 	for i := range keys {
-		t.Insert(keys[i], keys[i])
-	}
-
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			for i := range keys {
-				t.Lookup(keys[i])
-			}
+		v, ok := table.Lookup(keys[i])
+		if !ok || v != keys[i] {
+			t.Fatalf("Expected %q, got %q", keys[i], v)
 		}
-	})
-}
-
-func BenchmarkSyncMapR(b *testing.B) {
-	t := sync.Map{}
-	for i := range keys {
-		t.Store(keys[i], keys[i])
 	}
-
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			for i := range keys {
-				t.Load(keys[i])
-			}
-		}
-	})
-}
-
-func BenchmarkHtabW(b *testing.B) {
-	t := hashtable.New[string, string](size, hash.MemHashString64)
-	for i := range keys {
-		t.Insert(keys[i], keys[i])
-	}
-
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			for i := range keys {
-				t.Insert(keys[i], keys[i])
-			}
-		}
-	})
-}
-
-func BenchmarkSyncMapW(b *testing.B) {
-	t := sync.Map{}
-	for i := range keys {
-		t.Store(keys[i], keys[i])
-	}
-
-	b.ResetTimer()
-	b.RunParallel(func(p *testing.PB) {
-		for p.Next() {
-			for i := range keys {
-				t.Store(keys[i], keys[i])
-			}
-		}
-	})
 }
