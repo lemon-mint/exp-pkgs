b778675b02f8adf32b37410d6eca8a6c37a51ebe
lemon-mint <ice.lemons.mint@gmail.com>
2022-11-08 11:15:26 +0900

    Add WYRAND Mix

--- hash/hashutil/bloom/bloomf.go
+++ hash/hashutil/bloom/bloomf.go
@@ -56,6 +56,7 @@ func FromBytes(buf []byte) (*Bloom, error) {
 
 func hash(s []byte, i uint64) uint64 {
 	seed := splitmix64.Splitmix64(&i)
+	seed = wyhash.WYRAND(&seed)
 	return wyhash.Hash(s, seed)
 }
 
