a2e5619b7b8d7e7395c02cfd2a55e468c86498be
lemon-mint <ice.lemons.mint@gmail.com>
2022-11-13 12:09:07 +0900

    RandString

---
+++ util/randutil/randstr.go
@@ -0,0 +1,21 @@
+package randutil
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"v8.run/go/exp/fastrand"
+)
+
+func RandString(size int) string {
+	b := make([]byte, size)
+	_, err := rand.Read(b)
+	if err != nil {
+		// Fallback to a FastRandReader.
+		rng := fastrand.AcquireRNG()
+		fr := fastrand.FastRandReader{RNG: rng}
+		fr.Read(b)
+		rng.Release()
+	}
+	return base64.RawURLEncoding.EncodeToString(b)
+}
